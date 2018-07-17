d3bf148487873479bce02f579449db973ee24afb
Konce Quispe <konceq@fb.com>
2018-07-17 16:33:35 -0700

    Added calendar view with basic functionality

---
+++ app/src/main/java/e/rahmanapyrr/gift_app/CalendarActivity.java
@@ -0,0 +1,30 @@
+package e.rahmanapyrr.gift_app;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView myDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        calendarView = findViewById(R.id.myCalendar);
+        myDate = findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1 + 1) + "/" + i2 + "/" + i;
+                myDate.setText(date);
+            }
+        });
+    }
+}
