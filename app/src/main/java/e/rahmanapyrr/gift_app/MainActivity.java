fb4f0b4c64b3165c57e22ed9addd20ee674efae3
Aristides Peralta <hardyman123@fb.com>
2018-07-19 17:08:10 -0700

    update

--- app/src/main/java/e/rahmanapyrr/gift_app/MainActivity.java
+++ app/src/main/java/e/rahmanapyrr/gift_app/MainActivity.java
@@ -1,7 +1,10 @@
 package e.rahmanapyrr.gift_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import e.rahmanapyrr.gift_app.LogIn.LogInActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startup();
+
+
     }
+
+
+
+
+    public void startup(){
+        final Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
